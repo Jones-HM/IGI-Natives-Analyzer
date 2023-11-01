a4b274b8d30b213e20d1370cf8ccb5b82989c221
Haseeb <haseeb_heaven@yahoo.com>
2023-11-01 05:12:30 +0400

    Updated Graphs and calls

--- IGI-Calls-Analyzer.py
+++ IGI-Calls-Analyzer.py
@@ -1,21 +1,32 @@
 import os
 import re
 from graphviz import Digraph
+import argparse
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def analyze_file(file_path, visited_files=None, graph=None):
     if visited_files is None:
         visited_files = set()
 
     if file_path in visited_files or not os.path.isfile(file_path):
+        logger.info(f"File {file_path} already visited or does not exist.")
         return
 
     visited_files.add(file_path)
 
-    with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
-        content = file.read()
+    try:
+        with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
+            content = file.read()
+    except Exception as e:
+        logger.error(f"Error reading file {file_path}: {e}")
+        return
 
     # Extract function calls using regex
-    function_calls = re.findall(r'\bsub_[A-Za-z0-9_]+\b', content)
+    function_calls = re.findall(r'\b(sub_[A-Za-z0-9_]+|[A-Z][A-Za-z0-9_]+)\b', content)
     file_name = os.path.basename(file_path).split('.')[0]
     unique_function_calls = sorted(set(function_calls) - {file_name})
 
@@ -27,13 +38,13 @@ def analyze_file(file_path, visited_files=None, graph=None):
     unique_variables = sorted(set(variables))
 
     # Display results
-    print(f"File: {file_path}")
-    print("Function calls:")
+    logger.info(f"File: {file_path}")
+    logger.info("Function calls:")
     for func in unique_function_calls:
-        print(f"  {func}")
-    print("Variables:")
+        logger.info(f"  {func}")
+    logger.info("Variables:")
     for var in unique_variables:
-        print(f"  {var}")
+        logger.info(f"  {var}")
 
     # Add nodes and edges to the graph
     graph.node(file_name)
@@ -47,11 +58,27 @@ def analyze_file(file_path, visited_files=None, graph=None):
         func_file_path = os.path.join(directory_path, func_file)
         analyze_file(func_file_path, visited_files, graph)
 
-# Start analysis with the initial file
-initial_file_path = 'igi-ida-codes/FramesSet.c'
-graph = Digraph(comment='Function Calls')
-analyze_file(initial_file_path, graph=graph)
+# Create the parser
+parser = argparse.ArgumentParser(description='Analyze IGI calls.')
 
-# Save the graph to a file
-graph_file_path = initial_file_path.replace('.c','') + '_calls_graph'
-graph.render(graph_file_path, view=True, format='png')
+# Add an argument for the file path
+parser.add_argument('FilePath', metavar='filepath', type=str, help='the path to the file to analyze')
+
+def main():
+    try:
+        # Parse the arguments
+        args = parser.parse_args()
+
+        # Start analysis with the initial file
+        initial_file_path = args.FilePath
+        graph = Digraph(comment='Function Calls')
+        analyze_file(initial_file_path, graph=graph)
+
+        # Save the graph to a file
+        graph_file_path = initial_file_path.replace('.c','') + '_calls_graph'
+        graph.render(graph_file_path, view=True, format='png')
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
